94dcb44529f3220350bdaa3da5edde11b6a29c1b
Yaroslav-Marinich <ysmarinich@gmail.com>
2023-03-05 15:30:09 +0200

    add task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,43 @@
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  const promise = new Promise((fulfill, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+
+      if (shouldResolve) {
+        fulfill({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+
+  return promise;
+}
+
+form.addEventListener('submit', formSubmit);
+
+function formSubmit(event) {
+  event.preventDefault();
+
+  let stepValue = event.currentTarget.step.value;
+  let amountValue = event.currentTarget.amount.value;
+
+  for (let i = 1; i <= amountValue; i += 1) {
+    createPromise(i, stepValue * i)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
   }
+
 }

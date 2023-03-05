4bb48461369e4f966bb7cc8a5d22f322760c1589
Yaroslav-Marinich <ysmarinich@gmail.com>
2023-03-05 13:26:20 +0200

    add task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,20 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+let timerId = null;
+
+startBtn.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    const hexColor = getRandomHexColor();
+    document.querySelector('body').style.backgroundColor = hexColor;
+    startBtn.setAttribute('disabled', 'disabled');
+  }, 1000);
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(timerId);
+  startBtn.removeAttribute('disabled');
+});
